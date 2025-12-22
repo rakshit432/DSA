21649d730a1ed5fa67044e630a3ab682cbba8dbc
Rakshit <161947125+rakshit432@users.noreply.github.com>
2025-12-23 04:11:02 +0530

    Time: 4 ms (56.63%), Space: 13.6 MB (34.69%) - LeetHub

---
+++ 0960-delete-columns-to-make-sorted-iii/0960-delete-columns-to-make-sorted-iii.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int minDeletionSize(vector<string>& strs) {
+        int rows = strs.size();
+        int cols = strs[0].length();
+
+        vector<int> dp(cols, 1);
+        //dp[i] = LIS ending at index i
+
+        int LIS = 1;
+
+        //Khandani LIS Pattern
+        for(int i = 0; i < cols; i++) {
+            for(int j = 0; j < i; j++) {
+
+                bool valid = true;
+                for(int k = 0; k < rows; k++) {
+                    if(strs[k][j] > strs[k][i]) {
+                        valid = false;
+                        break;
+                    }
+                }
+
+                if(valid) {
+                    dp[i] = max(dp[i], dp[j]+1);
+                }
+
+            }
+
+            LIS = max(LIS, dp[i]);
+        }
+
+        return cols - LIS;
+
+    }
+};
