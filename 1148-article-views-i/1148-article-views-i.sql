b6bb7262c693d3b9c5922bc4261d55617998fb6d
Rakshit <161947125+rakshit432@users.noreply.github.com>
2026-08-25 00:38:26 +0530

    Time: 480 ms (36.76%), Space: 0B (100%) - LeetHub

--- 1148-article-views-i/1148-article-views-i.sql
+++ 1148-article-views-i/1148-article-views-i.sql
@@ -1,5 +1,6 @@
-# Write your MySQL query statement below
-select distinct author_id as id
-from views 
-where viewer_id = author_id 
-order by author_id asc ;
+SELECT author_id AS id
+FROM Views
+WHERE viewer_id = author_id
+GROUP BY author_id
+HAVING COUNT(*) >= 1
+ORDER BY id;
